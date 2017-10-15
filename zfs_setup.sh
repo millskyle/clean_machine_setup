67147497f7e4ff42f3d7863cca286024a757939d
Kyle Mills <kyle.mills@uoit.net>
2017-10-15 13:36:34 -0400

    Added NFS sharing setting and package instal

--- zfs_setup.sh
+++ zfs_setup.sh
@@ -1,5 +1,6 @@
 
 sudo apt-get install zfs
+sudo apt-get install nfs-kernel-server
 
 sudo zpool create -f archive raidz1 \
 /dev/disk/by-id/ata-ST8000NM0055-1RM112_ZA129DE5 \
@@ -9,6 +10,7 @@ cache /dev/nvme0n1p1
 
 sudo zfs create archive/01
 sudo zfs set compression=lz4 archive/01
+sudo zfs set sharenfs=on archive/01
 
 sudo addgroup archive
 sudo chgrp archive /archive/01
@@ -16,5 +18,4 @@ sudo chmod 755 /archive/01
 
 sudo adduser kmills archive
 sudo adduser itamblyn archive
-
 
