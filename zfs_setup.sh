31c5d5b284579d30247081fced1ecdc9785725bf
Kyle Mills <kyle.mills@uoit.net>
2018-06-14 15:56:34 -0400

    Update zfs_setup.sh

--- zfs_setup.sh
+++ zfs_setup.sh
@@ -12,6 +12,9 @@ sudo zfs create archive/01
 sudo zfs set compression=lz4 archive/01
 sudo zfs set sharenfs=on archive/01
 sudo zfs share -a
+echo "ZFS_SHARE=\'yes\'" | sudo tee -a /etc/default/zfs
+echo "ZFS_UNSHARE=\'yes\'" | sudo tee -a /etc/default/zfs
+
 
 sudo addgroup archive
 sudo chgrp archive /archive/01
@@ -20,5 +23,3 @@ sudo chmod 755 /archive/01
 sudo adduser kmills archive
 sudo adduser itamblyn archive
 
-echo "ZFS_SHARE=\'yes\'" | sudo tee -a /etc/default/zfs
-echo "ZFS_UNSHARE=\'yes\'" | sudo tee -a /etc/default/zfs
